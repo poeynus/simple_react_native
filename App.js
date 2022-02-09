8004d794082b9cc13e95bfa208479348619f4037
poeynus <poeynus@gmail.com>
2022-02-09 18:15:06 +0900

    tutorial is end

--- App.js
+++ App.js
@@ -6,106 +6,94 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Image,
+  Button,
+  TextInput,
+  ScrollView,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const IM = ({type}) => {
+  if (type === 'one') {
+    return (
+      <View>
+        <Image
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc-LaVCUX_5AZOncfaIrhQM7VE8LEIeuEk1w&usqp=CAU',
+          }}
+          style={styles.hearts}
+        />
+      </View>
+    );
+  } else if (type === 'two') {
+    return (
+      <View>
+        <Image
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiOCWCqFKx1tl7p0NRbNEPQPc9cf0SX_cXVQ&usqp=CAU',
+          }}
+          style={styles.hearts}
+        />
+      </View>
+    );
+  }
 };
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [test, setTest] = useState(false);
+  const [con, setCon] = useState('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onChangeTe = e => {
+    setCon(con);
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <Text style={{fontSize: 96}}>hihihi</Text>
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello World</Text>
+        <IM type="one" />
+        <IM type="two" />
+        <Button
+          onPress={() => {
+            setTest(!test);
+          }}
+          title={test ? 'push' : 'thank'}
+        />
+        <TextInput
+          defaultValue={con}
+          onChangeText={onChangeTe}
+          placeholder="input"
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  hello: {
+    color: 'red',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  hearts: {
+    width: 100,
+    height: 100,
   },
 });
 
